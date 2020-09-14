f06911d126227c793933229c58491e039c57e72b
Charles Hiller <charleshiller@Charless-MacBook-Pro.local>
2020-09-14 13:07:17 -0600

    Initial Commit

---
+++ src/build/precache-manifest.58943586358c93cb8d9cc7d28eb2c22a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e595c055e46ea1aec09059de9bff8ef2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e21d2c2f4100811578a",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "f7742bd7ca531cfad47f",
+    "url": "/static/js/2.b09d3ca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b09d3ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e21d2c2f4100811578a",
+    "url": "/static/js/main.ffa4b940.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
